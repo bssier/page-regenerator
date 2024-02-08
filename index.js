63ef1337ee5757ff2f14349ea1da5ee991e6ec26
Mihail Tavyrin <mihailtavyrin2@gmail.com>
2024-02-08 17:16:37 +0300

    install dependencies

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const { listen } = require("express/lib/application");
 const PORT = 3000
+const mongoose = require('mongoose')
 
 const app = express()
 
-const start = ()=>{
+const start = async ()=>{
   try{
+
     app.listen(PORT,() => {console.log(("server starting"))})
   }
   catch(e){
     console.log(e)
   }
 }
+
